31658a51739ce179e58cfc0b9d20d4a00274fcf4
Juan Pedro Garcia <pedroalberola3@gmail.com>
2024-10-16 18:01:48 +0200

    Añadiendo script con la explicación de la gráfica 'prometheus_up_metric.png'

---
+++ crear_explicacion_grafica.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Explicación de la gráfica 'prometheus_up_metric.png':"
+echo "----------------------------------------------"
+echo "Esta gráfica muestra el estado de las métricas 'up' en Prometheus."
+echo "La métrica 'up' indica si las instancias monitorizadas están activas."
+echo "En este caso, la gráfica refleja que la instancia monitoreada"
+echo "permanece en estado 'up', lo que significa que el servicio está activo."
+echo "Esta métrica es importante para garantizar la disponibilidad de los servicios."
+echo "----------------------------------------------"
